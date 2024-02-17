c311174f9e19a7f6187cc800cfc5903333da886e
rajantara <rajanpensas@gmail.com>
2024-02-18 00:41:14 +0800

    first message

---
+++ README.md
@@ -0,0 +1,6 @@
+# aplikasi-arsip-b2tke-pkl
+Aplikasi untuk pengarsipkan dokumen cetak agar lebih mudah dikelola di gudang maupun kantor
+<br>Username:
+<br>adminarsip
+<br>Password:
+<br>adminarsip
---
+++ include/footer.php
@@ -0,0 +1,9 @@
+<footer class="bg-dark text-white pt-3 mt-5">
+	<div class="container">
+		<div class="row text-center">
+			<div class="col">
+				<p>COPYRIGHT &copy; <?= date('Y'); ?> BALAI BESAR TEKNOLOGI KONVERSI ENERGI | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a></a></p>
+			</div>
+		</div>
+	</div>
+</footer>
